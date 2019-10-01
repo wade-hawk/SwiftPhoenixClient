0064d2360cb0c78c698b5a224ab906a0d99878af
wade.hawk <wade.hawk@kakaocorp.com>
2019-10-01 13:10:06 +0900

    supported macOS

---
+++ SwiftPhoenixClient macOS/SwiftPhoenixClient_macOS.h
@@ -0,0 +1,18 @@
+//
+//  SwiftPhoenixClient_macOS.h
+//  SwiftPhoenixClient macOS
+//
+//  Created by wade.hawk on 01/10/2019.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftPhoenixClient_macOS.
+FOUNDATION_EXPORT double SwiftPhoenixClient_macOSVersionNumber;
+
+//! Project version string for SwiftPhoenixClient_macOS.
+FOUNDATION_EXPORT const unsigned char SwiftPhoenixClient_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPhoenixClient_macOS/PublicHeader.h>
+
+
